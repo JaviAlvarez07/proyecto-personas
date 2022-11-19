ef7eb6a2b3a25d5f9b3bb9b7212a70d19f3ba14d
unknown <jagus_07@hotmail.com>
2022-11-19 18:59:22 -0300

    Rutas creadas

---
+++ src/app/personas/detalles-persona/detalles-persona.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-persona',
+  templateUrl: './detalles-persona.component.html',
+  styleUrls: ['./detalles-persona.component.css']
+})
+export class DetallesPersonaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/personas/persona-lista/persona-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona-lista',
+  templateUrl: './persona-lista.component.html',
+  styleUrls: ['./persona-lista.component.css']
+})
+export class PersonaListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
